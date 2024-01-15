2fc4ac807877cb07ad9ded0c080ff68993c543b2
azy <2543423832@qq.com>
2024-01-15 19:23:19 +0800

    [新增]新增线程池中任务队列属性

--- replayThreadPool/threadPool.h
+++ replayThreadPool/threadPool.h
@@ -1,11 +1,31 @@
 #ifndef __THREAD_POOL_H_
 #define __THREAD_POOL_H_
+
 #include <pthread.h>
+
+typedef struct task_t
+{
+    /* 钩子 - 回调函数. */
+    void *(*worker_hander)(void *arg);
+    /* 参数 */
+    void *arg;
+} task_t;
+
 /* 线程池结构体 */
 typedef struct threadpool_t
 {
+    /* 任务队列 */
+    task_t * taskQueue;
+    /* 任务队列的容量 */
+    int queueCapacity;
+    /* 任务队列的任务数 */
+    int queueSize;
+    /* 任务队列的队头 */
+    int queueFront;
+    /* 队伍队列的队尾 */
+    int queueRear;
+
     /* 线程池中的线程 */
-    pthread_t *threadId;
     pthread_t *threadIds;
 
     /* 最小的线程数 */
@@ -16,9 +36,8 @@ typedef struct threadpool_t
 
 /* 线程池初始化 */
 int threadPoolInit(threadpool_t *pool, int minThreads, int maxThreads);
+int threadPoolInit(threadpool_t *pool, int minThreads, int maxThreads, int queueCapacity);
 
 /* 线程池销毁 */
 int threadPoolDestroy(threadpool_t *pool);
-
-
 #endif // __THREAD_POOL_H_
